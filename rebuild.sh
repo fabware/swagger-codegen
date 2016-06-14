70dc5d6e8fa9417c0e95f95cc1d6c5f91301c50d
向灿 <xiang.can@gmail.com>
2016-06-14 15:51:57 +0800

    增加log HTTP请求用于测试的代码

---
+++ rebuild.sh
@@ -0,0 +1,3 @@
+mv modules/swagger-codegen-cli/target/swagger-codegen-cli.jar modules/swagger-codegen-cli/target/swagger-codegen-cli.jar.bak
+# rm modules/swagger-codegen-cli/target/swagger-codegen-cli.jar
+mvn package
